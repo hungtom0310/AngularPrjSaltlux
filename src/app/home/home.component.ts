19d7050a4e9934cb58313bd9445152bd4abc8e73
hungtom0310 <59992121+hungtom0310@users.noreply.github.com>
2022-08-31 08:46:14 +0700

    DATABD

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public name = 'Hưng';
+  public age = 40;
+
   constructor() {}
 
   ngOnInit(): void {}

1b42091a7468f2b02d4bc35b4040570131cf5558
hungtom0310 <59992121+hungtom0310@users.noreply.github.com>
2022-09-09 10:55:56 +0700

    1

--- src/app/tabs/tab-group/tab-group.component.ts
+++ src/app/tabs/tab-group/tab-group.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { TabPanelComponent } from '../tab-panel/tab-panel.component';
 
 @Component({
   selector: 'app-tab-group',
   templateUrl: './tab-group.component.html',
-  styleUrls: ['./tab-group.component.scss']
+  styleUrls: ['./tab-group.component.scss'],
 })
 export class TabGroupComponent implements OnInit {
+  tabPanelList: TabPanelComponent[] = [];
+  @Input() activeIndex = 0;
+  @Output() activeIndexChange = new EventEmitter<number>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  addTab(tab: TabPanelComponent) {
+    this.tabPanelList = [...this.tabPanelList, tab];
+    // this.tabPanelList.push(tab);
   }
 
+  removeTab(tab: TabPanelComponent) {
+    let found = -1;
+    this.tabPanelList = this.tabPanelList.filter((tp, index) => {
+      if (tp === tab) {
+        found = index;
+        return false;
+      }
+      return true;
+    });
+
+    if (found === this.activeIndex) {
+      // found=0
+      this.activeIndexChange.emit(
+        found === this.tabPanelList.length ? found - 1 : found
+      );
+    }
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
